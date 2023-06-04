fc6514131503dd08f4d509f2ae509e6253638a37
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-06-04 17:32:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Miura Anjin Ramen</h1>
+      <h2>Date</h2>
+      <h2>location widget</h2>
+      <footer>Link to Instagram in footer</footer>
+    </>
+  )
+}
