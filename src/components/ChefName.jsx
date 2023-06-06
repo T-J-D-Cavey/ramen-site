4ae0404341618c3fe1069fa02259bb1ef3eedcc2
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-06-06 18:11:44 +0100

    add reset styles, and background utility styles

--- src/components/ChefName.jsx
+++ src/components/ChefName.jsx
@@ -1,8 +1,8 @@
 export function ChefName() {
     return (
-        <div className='backgroundPictureOne'>
-            <div>
-                <h1>Miura Anjin Ramen</h1>
+        <div className='backgroundPictureOne flexContainerBackground'>
+            <div className='flexItemBackground'>
+                <h1>Miura<br />Anjin<br />Ramen</h1>
             </div>
         </div>
     )
