5928c29f5f954ab5490e7bfc71df8713422aed8b
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-06-06 19:24:45 +0100

    add styles

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,11 +1,13 @@
 export function Date() {
     return (
-        <div>
-            <h2>When:</h2>
-            <p>
-                <span>31/07/23</span><br />
-                Walk ins from 2pm <br />
-            </p>
+        <div className='backgroundPictureTwo flexContainerBackground'>
+            <div className='flexItemBackground'>
+                <h2>When:</h2>
+                <p>
+                    <span>31/07/23</span><br />
+                    Walk ins from 2pm <br />
+                </p>
+            </div>
         </div>
     )
 }
