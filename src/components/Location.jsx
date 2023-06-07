927a91b96415800378b7c21d76716e90d9eae67e
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-06-07 22:10:08 +0100

    finish version one styles

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,14 +2,13 @@ import {MapWidget} from './MapWidget';
 
 export function Location() {
     return (
-        <div className='flexContainerBigBackground column location'>
-            <div className='flexItemBackground address'>
+        <div className='flexContainerBigBackground column'>
+            <div className='flexItemBackground'>
                 <h2>Where:</h2>
                 <p>
                     <a href='https://thechristmassteps.com/' target='_blank'>
                     The Christmas Steps<br />
-                    Bristol<br />
-                    BS1 5BS
+                    Bristol, BS1 5BS
                     </a>
                 </p>
             </div>
